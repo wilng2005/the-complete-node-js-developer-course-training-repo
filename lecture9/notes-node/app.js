065d1bc9d9516ca5df46d90348d15bd60e5854c2
Wil Ng <wil.ng.2005@gmail.com>
2017-09-27 21:57:57 +0800

    Completed require of own module.

--- lecture9/notes-node/app.js
+++ lecture9/notes-node/app.js
@@ -1,16 +1,20 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.add(13,55);
+console.log(res);
 
-fs.appendFile(
-	'greetings.txt',
-	`Hello ${user.username}!`, 
-	error=>{
-		if(error){
-			console.log(error);
-		}
-	}
-);
+// var user = os.userInfo();
+
+// fs.appendFile(
+// 	'greetings.txt',
+// 	`Hello ${user.username}! You are ${notes.age}.`, 
+// 	error=>{
+// 		if(error){
+// 			console.log(error);
+// 		}
+// 	}
+// );
---
+++ lecture9/notes-node/notes.js
@@ -0,0 +1,13 @@
+console.log('Starting notes.js');
+// console.log(module);
+
+module.exports.age= 25;
+
+module.exports.add= (x, y)=>{
+	return x+y;
+};
+
+// module.exports.addNote = ()=>{
+// 	console.log("addNote");
+// 	return "New Note";
+// };
