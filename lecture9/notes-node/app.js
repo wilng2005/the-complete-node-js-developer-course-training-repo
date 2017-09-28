b48503b3a7bff091f0f2654d10bb00dad5d245f6
Wil Ng <wil.ng.2005@gmail.com>
2017-09-28 21:45:12 +0800

    Added in lodash

--- lecture9/notes-node/app.js
+++ lecture9/notes-node/app.js
@@ -3,9 +3,17 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-var res = notes.add(13,55);
-console.log(res);
+// console.log(_.isString(true));
+// console.log(_.isString('Wil'));
+
+var filteredArray=_.uniq(["Wil","Andrew","Wil",1,1,3,2,35,4]);
+
+console.log(filteredArray);
+
+// var res = notes.add(13,55);
+// console.log(res);
 
 // var user = os.userInfo();
 
