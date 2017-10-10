31e320434c04038ffb5bada7c31599ccc42e9372
Wil Ng <wil.ng.2005@gmail.com>
2017-10-10 22:29:59 +0800

    Finishing up with list notes, and cleaning up console.logs

--- lecture9/notes-node/app.js
+++ lecture9/notes-node/app.js
@@ -1,4 +1,3 @@
-console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
@@ -11,10 +10,6 @@ const argv = yargs.argv;
 
 var command = argv._[0];
 
-console.log("Command", command);
-//console.log("Process",process.argv);
-console.log("Yargs",argv);
-
 if (command === 'add'){
 	var note = notes.addNote(argv.title,argv.body);
 	if(note){
@@ -24,7 +19,10 @@ if (command === 'add'){
 		console.log("Note title already in use!");
 	}
 }else if (command === 'list'){
-	notes.getAll();
+	var allNotes = notes.getAll();
+	console.log(`Printing ${allNotes.length} notes.`);
+	allNotes.forEach(note=>notes.logNote(note));
+
 }else if(command === "read"){
 	var note = notes.getNote(argv.title);
 	if(note){
