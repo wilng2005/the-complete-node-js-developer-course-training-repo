b790bec24837a6968e0fed6c3a649339d7869b6e
Wil Ng <wil.ng.2005@gmail.com>
2017-10-14 21:58:08 +0800

    Async basics

---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log("Starting app");
+
+setTimeout(()=>{
+	console.log("Inside of callback.");
+},2000);
+
+setTimeout(()=>{
+	console.log("Second inside of callback.");
+},0);
+
+console.log("Finishing app");
+
