d47adb897006f9c0006a1d1b8e2ab252d5dbc4fd
Wil Ng <wil.ng.2005@gmail.com>
2017-10-16 18:11:11 +0800

    Adding in formatted address, lat, and lng

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
 	url:"https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lobard%20street%20philadeplphia",
 	json: true
 }, (error, response, body)=>{
-	console.log(JSON.stringify(body, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
 });
