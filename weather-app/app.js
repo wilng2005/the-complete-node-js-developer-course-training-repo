026c6026af19029412568a43d99f22d6f8a0d514
Wil Ng <wil.ng.2005@gmail.com>
2017-10-16 17:53:49 +0800

    Adding in pretty print

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
 	url:"https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lobard%20street%20philadeplphia",
 	json: true
 }, (error, response, body)=>{
-	console.log(body);
+	console.log(JSON.stringify(body, undefined, 2));
 });
