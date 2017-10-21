2bf12a99fe693c036c494b71bdeb934847c607de
Wil Ng <wil.ng.2005@gmail.com>
2017-10-21 22:33:23 +0800

    Fixing app myself before demo

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
 	.options({
@@ -14,22 +14,4 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-	url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json: true
-}, (error, response, body)=>{
-	if(error){
-		console.log("Unable to connect to Google server.");
-	}else if(body.status==="ZERO_RESULTS"){
-		console.log("Unable to find that address.");
-	}else if(body.status==="OK"){
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
-	}else{
-		console.log("Unknown :"+body.status);
-	}
-
-});
+geocode.geocode(argv.address);
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+var geocode=(address)=>{
+	var encodedAddress = encodeURIComponent(address);
+
+	request({
+		url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+		json: true
+	}, (error, response, body)=>{
+		if(error){
+			console.log("Unable to connect to Google server.");
+		}else if(body.status==="ZERO_RESULTS"){
+			console.log("Unable to find that address.");
+		}else if(body.status==="OK"){
+			console.log(`Address: ${body.results[0].formatted_address}`);
+			console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+			console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
+		}else{
+			console.log("Unknown status:"+body.status);
+		}
+
+	});
+};
+
+module.exports={
+	geocode
+};
