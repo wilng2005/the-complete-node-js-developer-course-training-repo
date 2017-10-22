821430c788710a583cc7a4aa8f8631684cccd859
Wil Ng <wil.ng.2005@gmail.com>
2017-10-22 17:26:23 +0800

    Attempting to hit darksky api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,41 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode.js');
+const constants = require('./config/constants.js');
+const request = require('request');
 
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-		}
-	})
-	.help()
-	.alias('help', 'h')
-	.argv;
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			describe: 'Address to fetch weather for',
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('help', 'h')
+// 	.argv;
 
-geocode.geocodeAddress(
-	argv.address,
-	(errorMessage,results)=>{
-		if(errorMessage){
-			console.log(errorMessage);
+// geocode.geocodeAddress(
+// 	argv.address,
+// 	(errorMessage,results)=>{
+// 		if(errorMessage){	
+// 			console.log(errorMessage);
+// 		}else{
+// 			console.log(JSON.stringify(results,undefined, 2));
+// 		}
+// 	}
+// );
+
+request({
+		url:`https://api.darksky.net/forecast/${constants.DARK_SKY_API_KEY}/37.8267,-122.4233`,
+		json: true
+	}, (error, response, body)=>{
+		if(!error&&response.statusCode===200){
+			console.log(body.currently.summary);
+			console.log(body.currently.temperature);
 		}else{
-			console.log(JSON.stringify(results,undefined, 2));
+			console.log("Unable to retrieve weather information.");
 		}
 	}
 );
