bbb4352220ec3098ba2cebcf9df02825ba6e6423
Wil Ng <wil.ng.2005@gmail.com>
2017-10-27 13:53:29 +0800

    Adding in minor tweaks

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -4,10 +4,11 @@ const constants = require('./config/constants.js');
 const argv = yargs
 	.options({
 		a: {
-			demand: true,
+			demand: false,
 			alias: 'address',
 			describe: 'Address to fetch weather for',
-			string: true
+			string: true,
+			default: "Changi South Ave 2"
 		}
 	})
 	.help()
@@ -26,7 +27,7 @@ axios.get(geocodeUrl)
 	var weatherUrl=`https://api.darksky.net/forecast/${constants.DARK_SKY_API_KEY}/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}?units=si`;
 	return axios.get(weatherUrl);
 }).then((response)=>{
-	console.log(`It's ${response.data.currently.temperature} but it feels like ${response.data.currently.apparentTemperature}. Sigh.`);
+	console.log(`It's ${response.data.currently.temperature} but it feels like ${response.data.currently.apparentTemperature}, with a ${response.data.currently.precipProbability*100}% chance of rain. Sigh.`);
 })
 .catch((e)=>{
 	if(e.code==='ENOTFOUND'){
