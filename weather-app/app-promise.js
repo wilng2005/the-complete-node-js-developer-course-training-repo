3b27dea0e3cf5d2d7a34aeca6944c5444e95678a
Wil Ng <wil.ng.2005@gmail.com>
2017-10-27 13:43:57 +0800

    Adding in the promisified version of the same app

---
+++ weather-app/app-promise.js
@@ -0,0 +1,37 @@
+const yargs = require('yargs');
+const axios = require('axios');
+const constants = require('./config/constants.js');
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+var encodedAddress =encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl)
+.then((response)=>{
+	if(response.data.status==='ZERO_RESULTS'){
+		throw new Error("Unable to find that address");
+	}
+	console.log(response.data.results[0].formatted_address);
+	var weatherUrl=`https://api.darksky.net/forecast/${constants.DARK_SKY_API_KEY}/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}?units=si`;
+	return axios.get(weatherUrl);
+}).then((response)=>{
+	console.log(`It's ${response.data.currently.temperature} but it feels like ${response.data.currently.apparentTemperature}. Sigh.`);
+})
+.catch((e)=>{
+	if(e.code==='ENOTFOUND'){
+		console.log("Unable to connect to API server.");
+	}else{
+		console.log(e.message);	
+	}
+});
--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,12 +1,33 @@
-var testPromise = new Promise((resolve , reject)=>{
-	setTimeout(()=>{
-		resolve('Hey it worked!');
-		// reject('Hey it didnt work!');
-	},2500);
+var asyncAdd = (a, b)=>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(typeof a === 'number' && typeof b === 'number'){
+				resolve(a + b);
+			}else{
+				reject("Arguments must be numbers.");
+			}
+		},1500);
+	});
+}
+
+asyncAdd(5,"7").then((result)=>{
+	console.log("Result", result);
+	return asyncAdd(result,33);
+}).then((result)=>{
+	console.log("Should be 45:", result);
+}).catch((error)=>{
+	console.log("Gah error! "+error);
 });
 
-testPromise.then((message)=>{
-	console.log("success: "+message);
-},(error)=>{
-	console.log("failure:"+error);
-});
+// var testPromise = new Promise((resolve , reject)=>{
+// 	setTimeout(()=>{
+// 		resolve('Hey it worked!');
+// 		// reject('Hey it didnt work!');
+// 	},2500);
+// });
+
+// testPromise.then((message)=>{
+// 	console.log("success: "+message);
+// },(error)=>{
+// 	console.log("failure:"+error);
+// });
