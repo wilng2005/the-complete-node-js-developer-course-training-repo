ccd88a1bde0172edef0d53d23af7539b47a8a72c
Wil Ng <wil.ng.2005@gmail.com>
2017-10-28 17:49:07 +0800

    Setting up express

---
+++ node-web-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/',(req, res)=>{
+	res.send({
+		name: "Zan Lozart Vazinki",
+		bio:{
+			height:123,
+			age:32
+		}
+	});
+});
+
+app.get('/about',(req, res)=>{
+	res.send("about me");
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		errorMessage:"An error has occurred. Again."
+	});
+});
+
+// app.disable('etag');
+
+app.listen(3000);
