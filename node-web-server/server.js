77b2a5a50a7e1105ee5ba1ae7a05f463fcc8a3b3
Wil Ng <wil.ng.2005@gmail.com>
2017-11-02 15:21:34 +0800

    Adding in middleware code

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+	// console.log(req);
+	var logMessage=new Date().toString()+":"+req.method+" on route "+req.url;
+	console.log(logMessage);
+	fs.appendFile("server.log",logMessage+"\n",(error)=>{if(error){console.log(error);}});
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('maintenance.hbs',{
+		pageTitle:"Website is down!",
+		welcomeMessage: "Sorry, but we're busy fixing something."
+	});
+});
+
 app.use(express.static(__dirname+"/public"));
 
 hbs.registerHelper('getCurrentYear', ()=>{
