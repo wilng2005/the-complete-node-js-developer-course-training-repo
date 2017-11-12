141f805f6336813aec32cb0e33d3b772366abf0a
Wil Ng <wil.ng.2005@gmail.com>
2017-11-12 15:23:05 +0800

    Did the challenge of async add and square

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,6 +1,18 @@
 module.exports.add=(x,y)=>x+y;
 
+module.exports.asyncAdd=(x,y, callback)=>{
+	setTimeout(()=>{
+		callback(x+y);
+	},1000);
+};
+
 module.exports.square=(x)=>x*x;
+
+module.exports.asyncSquare=(x,callback)=>{
+	setTimeout(()=>{
+		callback(x*x);
+	},500)
+};
 
 module.exports.setName=(user,name)=>{
 	let nameArr = name.split(" ");
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -6,9 +6,23 @@ it('should add two numbers',()=>{
 	expect(result).toBeA('number').toBe(8);
 });
 
+it('should async add two numbers', (done)=>{
+	utils.asyncAdd(5,32,(result)=>{
+		expect(result).toBe(37).toBeA('number');
+		done();
+	});
+});
+
 it('should square a number',()=>{
 	let result = utils.square(5);
 	expect(result).toBe(25).toBeA('number');
+});
+
+it('should async square a number',(done)=>{
+	utils.asyncSquare(6,(result)=>{
+		expect(result).toBe(36).toBeA('number');
+		done();
+	});
 });
 
 it('should set a user\'s name',()=>{
