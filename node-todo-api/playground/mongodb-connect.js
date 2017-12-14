98212ab376e33fa60d3230016b1c4998259deada
Wil Ng <wil.ng.2005@gmail.com>
2017-12-14 09:37:55 +0800

    Playground for mongo db

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+
+	if(err){
+		return console.log('Unable to connect to MongoDB server');
+	}
+
+	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').insertOne({
+	// 	text: 'Something to do',
+	// 	completed: false
+	// },(err,result)=>{
+	// 	if(err){
+	// 		return console.log('Unable to insert todo', err);
+	// 	}
+
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+
+	db.collection('Users').insertOne({
+		name: 'Fandico Lamar',
+		age: 44,
+		location: 'Jamacoland'
+	},(err,result)=>{
+		if(err){
+			return console.log('Unable to insert user', err);
+		}
+
+		console.log(JSON.stringify(result.ops, undefined, 2));
+		console.log(result.ops[0]._id.getTimestamp());
+	});
+
+	db.close();
+});
