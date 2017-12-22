98920f7aeaeaf8eb345c2b19351d124176934f97
Wil Ng <wil.ng.2005@gmail.com>
2017-12-22 09:39:28 +0800

    Adding in user model

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,24 +5,58 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo',{
 	text:{
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
 	},
 	completed:{
-		type: Boolean
+		type: Boolean,
+		default: false
 	},
 	completedAt:{
-		type: Number
+		type: Number,
+		default: null
 	}
 });
 
 var newTodo = new Todo({
-	text: 'Cook supper',
-	completed: true,
-	completedAt: 130783
+	text:true
 });
 
-newTodo.save().then((doc)=>{
-	console.log('Saved todo', doc);
-},(e)=>{
-	console.log('Unable to save todo');
+// newTodo.save().then((doc)=>{
+// 	console.log('Saved todo', doc);
+// },(e)=>{
+// 	console.log('Unable to save todo');
+// 	console.log(e);
+// });
+
+// new user model
+// email / required/ trimmed / string /minlength 1
+// 
+
+var User = mongoose.model('User',{
+	name:{
+		type: String,
+		required: true,
+		minlength: 1,
+		trim:true
+	},
+	email:{
+		type: String,
+		required: true,
+		trim: true,
+		minlength:1
+	}
+}); 
+
+var newUser = new User({
+	name: "Wil Ng",
+	email: "wil.ng.2005@gmail.com"
+});
+
+newUser.save().then((doc)=>{
+	console.log(JSON.stringify(doc,undefined,2));
+},(err)=>{
+	console.log(err);
 });
